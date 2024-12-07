2f6ed1558f9a8b36972af8f531f36abec9c7c716
yeioiwe <yolog998@protonmail.com>
2024-12-07 10:49:51 +0100

    upd

--- backend/apps/admin/admin.main.ts
+++ backend/apps/admin/admin.main.ts
@@ -4,7 +4,13 @@ import { AdminModule } from './admin.module';
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
   //TODO .env port
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://109.120.139.129:3000',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   await app.listen(8088);
 }
 
--- backend/apps/api/api.main.ts
+++ backend/apps/api/api.main.ts
@@ -3,7 +3,12 @@ import { ApiModule } from './api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://109.120.139.129:3000',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   //TODO .env port
   await app.listen(8080);
 }
