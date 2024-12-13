c224c2de1f8b95e2a7fcd0a4984187fd92ecfd44
yeioiwe <yolog998@protonmail.com>
2024-12-13 04:22:21 +0100

    jwt module to user module

--- backend/apps/admin/user/user.module.ts
+++ backend/apps/admin/user/user.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HistoryEntity } from 'apps/libs/db/entity/history.entity';
 import { UserEntity } from 'apps/libs/db/entity/user.entity';
@@ -6,7 +7,7 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity, HistoryEntity])],
+    imports: [TypeOrmModule.forFeature([UserEntity, HistoryEntity]), JwtModule],
     providers: [UserService],
     controllers: [UserController],
 })
