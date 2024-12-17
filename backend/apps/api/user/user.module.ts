b480986c798a803bf046e1dad067fb8829bc66d6
yeioiwe <yolog998@protonmail.com>
2024-12-17 10:54:25 +0100

    user module

--- backend/apps/api/user/user.module.ts
+++ backend/apps/api/user/user.module.ts
@@ -3,11 +3,12 @@ import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HistoryEntity } from 'apps/libs/db/entity/history.entity';
 import { UserEntity } from 'apps/libs/db/entity/user.entity';
+import { WalletEntity } from 'apps/libs/db/entity/wallet.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity, HistoryEntity])],
+    imports: [TypeOrmModule.forFeature([UserEntity, HistoryEntity, WalletEntity])],
     providers: [UserService, JwtService],
     controllers: [UserController],
 })
